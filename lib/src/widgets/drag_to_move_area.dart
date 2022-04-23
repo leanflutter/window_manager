faefdce1f50a23698d829d59a30066a7c7526dcf
LiJianying <lijy91@foxmail.com>
2022-04-23 16:27:43 +0800

    Add DragToMoveArea widget doc #82

--- lib/src/widgets/drag_to_move_area.dart
+++ lib/src/widgets/drag_to_move_area.dart
@@ -2,6 +2,24 @@ import 'package:flutter/material.dart';
 
 import '../window_manager.dart';
 
+/// A widget for drag to move window.
+///
+/// When you have hidden the title bar, you can add this widget to move the window position.
+///
+/// {@tool snippet}
+///
+/// The sample creates a red box, drag the box to move the window.
+///
+/// ```dart
+/// DragToMoveArea(
+///   child: Container(
+///     width: 300,
+///     height: 32,
+///     color: Colors.red,
+///   ),
+/// )
+/// ```
+/// {@end-tool}
 class DragToMoveArea extends StatelessWidget {
   final Widget child;
 
@@ -9,6 +27,7 @@ class DragToMoveArea extends StatelessWidget {
     Key? key,
     required this.child,
   }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
