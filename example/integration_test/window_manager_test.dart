f5176b7600c8dd1858a016d6ad3342fe81eb3f9a
J-P Nurmi <jpnurmi@gmail.com>
2023-01-05 12:56:12 +0100

    Rename integration tests
    It's better to test setters in separate test methods to make it possible
    to skip them per platform depending on how we are able to make them work
    on each platform or not.

--- example/integration_test/window_manager_test.dart
+++ example/integration_test/window_manager_test.dart
@@ -19,88 +19,88 @@ Future<void> main() async {
     },
   );
 
-  testWidgets('bounds', (tester) async {
+  testWidgets('getBounds', (tester) async {
     expect(await windowManager.getBounds(),
         isA<Rect>().having((r) => r.size, 'size', Size(640, 480)));
   });
 
-  testWidgets('always on bottom', (tester) async {
+  testWidgets('isAlwaysOnBottom', (tester) async {
     expect(await windowManager.isAlwaysOnBottom(), isFalse);
   }, skip: Platform.isMacOS || Platform.isWindows);
 
-  testWidgets('always on top', (tester) async {
+  testWidgets('isAlwaysOnTop', (tester) async {
     expect(await windowManager.isAlwaysOnTop(), isFalse);
   });
 
-  testWidgets('closable', (tester) async {
+  testWidgets('isClosable', (tester) async {
     expect(await windowManager.isClosable(), isTrue);
   });
 
-  testWidgets('focused', (tester) async {
+  testWidgets('isFocused', (tester) async {
     expect(await windowManager.isFocused(), isTrue);
   });
 
-  testWidgets('fullscreen', (tester) async {
+  testWidgets('isFullScreen', (tester) async {
     expect(await windowManager.isFullScreen(), isFalse);
   });
 
-  testWidgets('has shadow', (tester) async {
+  testWidgets('hasShadow', (tester) async {
     expect(await windowManager.hasShadow(), isTrue);
   }, skip: Platform.isLinux);
 
-  testWidgets('maximizable', (tester) async {
+  testWidgets('isMaximizable', (tester) async {
     expect(await windowManager.isMaximizable(), isTrue);
   }, skip: Platform.isMacOS);
 
-  testWidgets('maximized', (tester) async {
+  testWidgets('isMaximized', (tester) async {
     expect(await windowManager.isMaximized(), isFalse);
   });
 
-  testWidgets('minimizable', (tester) async {
+  testWidgets('isMinimizable', (tester) async {
     expect(await windowManager.isMinimizable(), isTrue);
   }, skip: Platform.isMacOS);
 
-  testWidgets('minimized', (tester) async {
+  testWidgets('isMinimized', (tester) async {
     expect(await windowManager.isMinimized(), isFalse);
   });
 
-  testWidgets('movable', (tester) async {
+  testWidgets('isMovable', (tester) async {
     expect(await windowManager.isMovable(), isTrue);
   }, skip: Platform.isLinux || Platform.isWindows);
 
-  testWidgets('opacity', (tester) async {
+  testWidgets('getOpacity', (tester) async {
     expect(await windowManager.getOpacity(), 1.0);
   });
 
-  testWidgets('position', (tester) async {
+  testWidgets('getPosition', (tester) async {
     expect(await windowManager.getPosition(), isA<Offset>());
   });
 
-  testWidgets('prevent close', (tester) async {
+  testWidgets('isPreventClose', (tester) async {
     expect(await windowManager.isPreventClose(), isFalse);
   });
 
-  testWidgets('resizable', (tester) async {
+  testWidgets('isResizable', (tester) async {
     expect(await windowManager.isResizable(), isTrue);
   });
 
-  testWidgets('size', (tester) async {
+  testWidgets('getSize', (tester) async {
     expect(await windowManager.getSize(), Size(640, 480));
   });
 
-  testWidgets('skip taskbar', (tester) async {
+  testWidgets('isSkipTaskbar', (tester) async {
     expect(await windowManager.isSkipTaskbar(), isFalse);
   }, skip: Platform.isWindows);
 
-  testWidgets('title', (tester) async {
+  testWidgets('getTitle', (tester) async {
     expect(await windowManager.getTitle(), 'window_manager_test');
   });
 
-  testWidgets('title bar height', (tester) async {
+  testWidgets('getTitleBarHeight', (tester) async {
     expect(await windowManager.getTitleBarHeight(), isNonNegative);
   });
 
-  testWidgets('visible', (tester) async {
+  testWidgets('isVisible', (tester) async {
     expect(await windowManager.isVisible(), isTrue);
   });
 }
